11e06b233acc6a8f8fcf0841a86f6a08283ce2f3
FannyLu163 <lsf1120@msn.com>
2026-06-25 08:49:43 +0000

    防止重複報名活動時的錯誤訊息更新

--- src/app.py
+++ src/app.py
@@ -64,7 +64,7 @@ def signup_for_activity(activity_name: str, email: str):
 
     # Prevent duplicate signups
     if email in activity["participants"]:
-        raise HTTPException(status_code=400, detail="Student is already signed up for this activity")
+        raise HTTPException(status_code=400, detail="Student is already signed up for this activity!")
 
     # Add student
     activity["participants"].append(email)
